3621bb1dcc6ac44dc28f328d013c6b0ebf847131
Konstantin Protzen <code@kondev.de>
2024-10-18 18:19:16 +0200

    🎉 Add nav and grid

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/head.html'; ?>
+</head>
+
+<body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/navbar.html'; ?>
+    <div class="menu"></div>
+    <main></main>
+    <footer></footer>
+</body>
+
+</html>
