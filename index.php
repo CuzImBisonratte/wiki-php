1c2cf75783f98ffd2941a797aee6e2e4c3c1fd24
Konstantin Protzen <code@kondev.de>
2024-10-18 21:08:50 +0200

    ✨ Add menu

--- index.php
+++ index.php
@@ -10,8 +10,24 @@
 
 <body>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/res/html/navbar.html'; ?>
-    <div class="menu"></div>
-    <main></main>
+    <!-- TODO: Replace menu with menu generator PHP script -->
+    <div class="menu">
+        <div class="menu__item">
+            <a class="menu__link" href="/">Intro</a>
+        </div>
+        <div class="menu__item">
+            <div class="menu__itemgroup">
+                <div class="menu__item menu__itemgroup__title">
+                    <a class="menu__link" href="/endpoints">Endpoints</a>
+                </div>
+                <div class="menu__item menu__itemgroup__item menu__item--active">
+                    <a class="menu__link" href="/endpoints/">Classes</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <main>
+    </main>
     <footer></footer>
 </body>
 
